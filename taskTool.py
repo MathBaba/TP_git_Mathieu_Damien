6b471aef5968404078b9b05a967b03bf4208b1e3
MathBaba <mathieubaba492@gmail.com>
2024-03-07 12:19:04 +0400

    fonction de marquage d'une tache comme terminee

--- taskTool.py
+++ taskTool.py
@@ -58,3 +58,32 @@ def supprimer_tache_json(nom_fichier, titre_tache):
     return liste_taches
 
 supprimer_tache_json("")
+
+
+import json
+
+def marquer_tache_terminee(nom_fichier, titre_tache):
+   
+    try:
+       
+        with open(nom_fichier, 'r') as fichier:
+            liste_taches = json.load(fichier)
+    except FileNotFoundError:
+        print(f"Le fichier {nom_fichier} n'existe pas.")
+        return
+
+    if titre_tache in liste_taches:
+        
+        liste_taches[titre_tache]["completee"] = True
+        print(f"Tâche '{titre_tache}' marquée comme terminée.")
+    else:
+        print(f"Tâche '{titre_tache}' non trouvée dans la liste.")
+
+   
+    with open(nom_fichier, 'w') as fichier:
+        json.dump(liste_taches, fichier, indent=2)
+
+    return liste_taches
+
+
+marquer_tache_terminee("")
