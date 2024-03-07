f00a0ee785f538bf78543439efc3159166052793
DamienHS <damienkikou262122@icloud.com>
2024-03-07 11:53:50 +0400

    Ajout de la fonction pour charger un fichier JSON

--- dataTask.py
+++ dataTask.py
@@ -3,3 +3,8 @@ import json
 def save_to_json(data, filename):
     with open(filename, 'w') as json_file:
         json.dump(data, json_file, indent=2)
+
+def load_from_json(filename):
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+    return data
