f4032df3560b258b039b81f5205bf2244c2d60a4
MathBaba <mathieubaba492@gmail.com>
2024-03-07 12:09:41 +0400

    fonction de suppression d'une tache dans le dictionnaire

--- taskTool.py
+++ taskTool.py
@@ -30,3 +30,31 @@ def ajouter_tache_json(nom_fichier, titre, description, date_echeance):
 
 
 ajouter_tache_json("")
+
+import json
+
+def supprimer_tache_json(nom_fichier, titre_tache):
+    
+    try:
+        # Charge le contenu du fichier JSON existant
+        with open(nom_fichier, 'r') as fichier:
+            liste_taches = json.load(fichier)
+    except FileNotFoundError:
+        print(f"Le fichier {nom_fichier} n'existe pas.")
+        return
+
+    
+    if titre_tache in liste_taches:
+        
+        del liste_taches[titre_tache]
+        print(f"Tâche '{titre_tache}' supprimée.")
+    else:
+        print(f"Tâche '{titre_tache}' non trouvée dans la liste.")
+
+    
+    with open(nom_fichier, 'w') as fichier:
+        json.dump(liste_taches, fichier, indent=2)
+
+    return liste_taches
+
+supprimer_tache_json("")
